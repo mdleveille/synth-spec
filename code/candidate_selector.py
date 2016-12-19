b34994fbf0636a81216841b218b007b4198e43bf
Michael Leveille <leveille1313@gmail.com>
2016-12-19 15:54:02 -0700

    combine to one file that rebins data and calculates flux at d

--- code/candidate_selector.py
+++ code/candidate_selector.py
@@ -1,44 +1,45 @@
 "Read flux data .fits files and save 0.5 M_sun M-dwarf and 1.3 BSS"
 
 # load packages and values
+import numpy as np
+import matplotlib.pyplot as plt 
 from astropy.io import fits
 from astropy import units as u 
-import numpy as np
 from astropy.constants import M_sun
 from astropy.constants import L_sun
-import matplotlib.pyplot as plt 
-from pysynphot import observation
-from pysynphot import spectrum
+from pysynphot import observation                                                # must cite pysynphot; see notebook page 10
+from pysynphot import spectrum                                                   # must cite pysynphot; see notebook page 10
 
 # load wavelength grid	
-wave_hdu = fits.open('/Users/michael/synth_spec/flux_data/WAVE_PHOENIX-ACES-AGSS-COND-2011.fits')
-wave_data = wave_hdu[0].data         # in angstroms
+wave_hdu  = fits.open('../flux_data/WAVE_PHOENIX-ACES-AGSS-COND-2011.fits')
+wave_data = wave_hdu[0].data                                                     # in angstroms
 
 # manipulate parameters
-d = 1                 # distance at which we calculate fluxin parsecs
-xmin = 5180           # in angstroms
-xmax = 5190           # in angstroms
+d = 300                                                                            # distance at which we calculate flux (in parsecs)
+xmin = 15830                                                                     # (for plotting) in angstroms
+xmax = 15950                                                                     # (for plotting) in angstroms
 
 # set constants and create lists
-M_sun = M_sun.value
+M_sun   = M_sun.value
 L_sun_W = L_sun.value
-L_sun = L_sun_W*1e7
+L_sun   = L_sun_W*1e7
 
 # Create empty arrays to be filled later
-Mdwarfs = []
+Mdwarfs      = []
+BSSs         = []
+suns         = []
 Flux_Mdwarfs = []
-BSSs = []
-Flux_BSSs = []
-suns = []
+Flux_BSSs    = []
 
-# genfromtxt works better than loadtxt; it keeps the filename as a string
-files = np.genfromtxt('/Users/mleveille/synth_spec/flux_data/flux_data.txt',dtype=str)
+# read in a list of all 841 fits files; candidate stars will be selected from this list 
+## genfromtxt works better than loadtxt; it keeps the filename as a string
+files = np.genfromtxt('../flux_data/flux_data.txt',dtype=str)
 
-# this loop reads in all the fits files and evaluates their masses 
-##(the first 'if' statement excludes a file that is missing the keyword 'PHXMASS')
+# this loop opens all the fits files and evaluates their masses then makes lists of candidate stars
+##the first 'if' statement excludes a file that is missing the keyword 'PHXMASS'
 for i in np.arange(0,len(files)-1):
 	if files[i] != 'lte05300-0.00-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits':
-		hdu = fits.open("/Users/mleveille/synth_spec/flux_data/"+str(files[i]))
+		hdu = fits.open("../flux_data/"+str(files[i]))
 		M = hdu[0].header['PHXMASS']/1000 # in kg
 
 		# selects ~0.5 M_sun M-dwarf candidate files
@@ -57,34 +58,27 @@ for i in np.arange(0,len(files)-1):
 
 		hdu.close()
 
-# this loop calculates the flux detected at a distance, d, for the M-dwarfs
-for i in np.arange(0,len(Mdwarfs)):
-	# loads relevant numbers from fits headers
-	hdu = fits.open("/Users/mleveille/synth_spec/flux_data/"+str(Mdwarfs[i]))
-	F_stellar_surface = hdu[0].data*1e-8                                         # in erg/s/cm^2/angstrom
-	R_eff = hdu[0].header['PHXREFF']                                             # in cm
-	
-	# calculate the flux a distance, d, from the star (FOR M-DWARFS)
-	d_cm = u.pc.to(u.cm,d)
-	F_earth = F_stellar_surface*(R_eff/d_cm)**2
-	Flux_Mdwarfs.append(F_earth)
-	hdu.close()
+# save list of Mdwarfs to text file (Mdwarf_list.txt in star_arrays directory)
+file = open('../star_arrays/Mdwarf_list.txt', 'w')
+for i in np.arange(0,len(Mdwarfs)-1):
+	file.write("%s \n" % Mdwarfs[i])
+file.close()
 
-# this loop calculates the flux detected at a distance, d, for the BSSs
-for i in np.arange(0,len(BSSs)):
-	# loads relevant numbers from fits headers
-	hdu = fits.open("/Users/mleveille/synth_spec/flux_data/"+str(BSSs[i]))
-	F_stellar_surface = hdu[0].data*1e-8                                         # in erg/s/cm^2/angstrom
-	R_eff = hdu[0].header['PHXREFF']                                             # in cm
-	
-	# calculate the flux at a distance, d, from the star (FOR BSSs)
-	d_cm = u.pc.to(u.cm,d)
-	F_earth = F_stellar_surface*(R_eff/d_cm)**2
-	Flux_BSSs.append(F_earth)
-	hdu.close()
+# save list of BSSs to text file (BSSs_list.txt in star_arrays directory)
+file = open('../star_arrays/BSSs_list.txt', 'w')
+for i in np.arange(0,len(BSSs)-1):
+	file.write("%s \n" % BSSs[i])
+file.close()
 
-# # loop to print values for stellar effective temperature and luminosity of candidate stars
-# ## to determine which stars are on the main sequence
+# save list of suns to text file (suns_list.txt in star_arrays directory)
+file = open('../star_arrays/suns_list.txt', 'w')
+for i in np.arange(0,len(suns)-1):
+	file.write("%s \n" % suns[i])
+file.close()
+
+# # FOR HR DIAGRAM MATCHING (to determine what's on the main sequence)
+# ## loop to print values for stellar effective temperature and luminosity of candidate stars
+# ### to determine which stars are on the main sequence
 # for i in np.arange(0,len(Mdwarfs)):
 # 	hdu = fits.open("/Users/mleveille/synth_spec/flux_data/"+str(Mdwarfs[i]))
 # 	T = hdu[0].header['PHXTEFF']
@@ -100,18 +94,27 @@ for i in np.arange(0,len(BSSs)):
 #	hdu.close()
 
 # ********** BEST MAIN SEQUENCE FITS ARE:
-MS_BSS    = BSSs[5]
-MS_Mdwarf = Mdwarfs[3]
+MS_BSS       = BSSs[5]
+MS_Mdwarf    = Mdwarfs[3]
 solar_analog = suns[50]
 
-# input the desired file for plotting in flux_file
-flux_file = MS_BSS                                                               # CHANGE THIS TO PLOT DIFFERENT STARS
-plot_hdu = fits.open('/Users/mleveille/synth_spec/flux_data/'+str(flux_file))
-flux_data = plot_hdu[0].data
+# make flux arrays for best candidate stars
+MS_Mdwarf_hdu       = fits.open('../flux_data/'+str(MS_Mdwarf))
+MS_Mdwarf_flux_data = MS_Mdwarf_hdu[0].data*1e-8                                 # in erg/s/cm^2/angstrom
 
-# resample data to lower resolution from 100,000 to 40,000
-## create lo-res wavelength grid with 
-lo_res_wav = np.arange(500,50000,0.0375)     
+MS_BSS_hdu       = fits.open('../flux_data/'+str(MS_BSS))
+MS_BSS_flux_data = MS_BSS_hdu[0].data*1e-8                                       # in erg/s/cm^2/angstrom
+
+solar_analog_hdu       = fits.open('../flux_data/'+str(solar_analog))
+solar_analog_flux_data = solar_analog_hdu[0].data*1e-8                           # in erg/s/cm^2/angstrom
+
+# # general for any file (need to define file = )
+# plot_hdu = fits.open('../flux_data/'+str(file))
+# flux_data = plot_hdu[0].data*1e-8                                              # in erg/s/cm^2/angstrom
+
+# rebin data to lower resolution from R = 100,000 to R = 40,000
+## create lo-res wavelength grid with binsize=0.0375nm
+lo_res_wav = np.arange(xmin,xmax,0.375)                                        # see notebook pg. 12 for calculation of this binsize 
 
 def rebin_spec(wave, specin, wavnew):                                            # wavnew is the new wave grid
 	spec = spectrum.ArraySourceSpectrum(wave=wave, flux=specin)                  # configures wave/spectrum for pysynphot
@@ -120,44 +123,43 @@ def rebin_spec(wave, specin, wavnew):                                           
 	obs = observation.Observation(spec, filt, binset=wavnew, force='taper')
 	return obs.binflux
 
-new_binflux = rebin_spec(wave_data,flux_data,lo_res_wav)
+MS_Mdwarf_binflux_data    = rebin_spec(wave_data,MS_Mdwarf_flux_data,lo_res_wav)         # this is the re-binned flux array 
+MS_BSS_binflux_data       = rebin_spec(wave_data,MS_BSS_flux_data,lo_res_wav)            # this is the re-binned flux array 
+solar_analog_binflux_data = rebin_spec(wave_data,solar_analog_flux_data,lo_res_wav)
+
+# calculate flux at distance d, from stellar surface
+Mdwarf_Reff       = MS_Mdwarf_hdu[0].header['PHXREFF']                                 # effective stellar radius of the best-fit Mdwarf in cm
+BSS_Reff          = MS_BSS_hdu[0].header['PHXREFF']                                    # effective stellar radius of the best-fit BSS in cm
+solar_analog_Reff = solar_analog_hdu[0].header['PHXREFF']
+
+d_cm = u.pc.to(u.cm,d)
+
+MS_Mdwarf_final_flux    = MS_Mdwarf_binflux_data*(Mdwarf_Reff/d_cm)**2
+MS_BSS_final_flux       = MS_BSS_binflux_data*(BSS_Reff/d_cm)**2
+solar_analog_final_flux = solar_analog_binflux_data*(solar_analog_Reff/d_cm)**2
 
 # make a plot
-plt.plot(wave_data,plot_hdu[0].data,'bo')
-plt.plot(lo_res_wav,new_binflux,'ro')
+## define x-axis range
+#xmin = 14995                                                                     # in angstroms
+#xmax = 15005                                                                     # in angstroms
+
+plt.clf()
+
+plt.figure(1)
+plt.plot(lo_res_wav,MS_BSS_final_flux,'r-')
 plt.xlabel(r'Wavelength, [$\AA$]')
 plt.xlim(xmin,xmax)
 plt.ylabel(r'Flux, [$erg / s / cm^{2} / \AA$]')
-plt.title(r'$T_{eff} = %dK, M = %.2f M_{\odot}, log(g) = %.2f$' % (plot_hdu[0].header['PHXTEFF'],plot_hdu[0].header['PHXMASS']/1000/M_sun,plot_hdu[0].header['PHXLOGG']))
+plt.title(r'$T_{eff} = %dK, M = %.2f M_{\odot}, log(g) = %.2f$' % (MS_BSS_hdu[0].header['PHXTEFF'],MS_BSS_hdu[0].header['PHXMASS']/1000/M_sun,MS_BSS_hdu[0].header['PHXLOGG']))
+
+# plt.figure(2)
+# plt.plot(wave_data,MS_BSS_flux_data,'b-')
+# plt.xlabel(r'Wavelength, [$\AA$]')
+# plt.xlim(xmin,xmax)
+# plt.ylabel(r'Flux, [$erg / s / cm^{2} / \AA$]')
+# plt.title(r'$T_{eff} = %dK, M = %.2f M_{\odot}, log(g) = %.2f$' % (MS_BSS_hdu[0].header['PHXTEFF'],MS_BSS_hdu[0].header['PHXMASS']/1000/M_sun,MS_BSS_hdu[0].header['PHXLOGG']))
+
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
